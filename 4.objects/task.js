3a8b6320d2f87784ebb62c52941d0d97be3c1fc5
Anton Lysenko <antonlysenko161@yandex.ru>
2024-03-09 14:57:01 +0300

    Выполнено ДЗ по занятию Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+    this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
 }
 
+let student1 = new Student("Антон", "мужской", 24);
+let student2 = new Student("Мария", "женский", 24)
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+    if (this.hasOwnProperty("marks"))
+        this.marks.push(...marks);
+    }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.hasOwnProperty("marks") === false || this.marks.length === 0){
+        return 0;
+    } else {
+        let sum = this.marks.reduce((acc, item, index, arr) => {
+            acc += item;
+            if(index === arr.length - 1){
+                return acc/arr.length;
+            }
+            return acc;
+        }, 0)
+        return sum;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
